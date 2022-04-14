000cad3e29f79521d60e6a6b7b959324003bb0aa
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-04-15 06:25:47 +0700

    Test clang

--- download.sh
+++ download.sh
@@ -8,4 +8,4 @@ git clone --depth 1 https://github.com/NFS-projects/gcc-arm -b 12.x $CIRRUS_WORK
 git clone --depth 1 https://github.com/NFS-projects/gcc-arm64 -b 12.x $CIRRUS_WORKING_DIR/GCC64
 git clone --depth 1 https://github.com/NFS-projects/gcc-arm -b 11.x $CIRRUS_WORKING_DIR/GCC-11-32
 git clone --depth 1 https://github.com/NFS-projects/gcc-arm64 -b 11.x $CIRRUS_WORKING_DIR/GCC-11-64
-git clone --depth 1 https://github.com/NFS-projects/NFS-clang -b main $CIRRUS_WORKING_DIR/CLANG
+git clone --depth 1 https://gitlab.com/AnGgIt86/nfs-clang.git -b main $CIRRUS_WORKING_DIR/CLANG
