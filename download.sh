6eac11df50dcbc461ed7dab9af66f231cbbee80d
NFS86 <jarbull86@gmail.com>
2022-06-22 22:23:59 +0000

    remove clang branch

--- download.sh
+++ download.sh
@@ -4,4 +4,4 @@ echo "Downloading few Dependecies . . ."
 # Kernel Sources
 git clone --depth 1 https://github.com/baunilla/android_kernel_xiaomi_rosy $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
-git clone --depth 1 https://github.com/kdrag0n/proton-clang -b main $CIRRUS_WORKING_DIR/CLANG
+git clone --depth 1 https://github.com/kdrag0n/proton-clang $CIRRUS_WORKING_DIR/CLANG
