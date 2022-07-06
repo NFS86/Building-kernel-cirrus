c282c9d42e32b3256f50bd93c95cc2801199927e
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-07-06 20:48:59 +0700

    Back to NFS clang

--- download.sh
+++ download.sh
@@ -4,4 +4,4 @@ echo "Downloading Kernel Sources.."
 git clone --depth 1 https://github.com/NFS-projects/android_kernel_xiaomi_rosy $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 echo ""
 echo "Downloading Toolchain.."
-git clone --depth 1 https://github.com/kdrag0n/proton-clang $CIRRUS_WORKING_DIR/CLANG
+git clone --depth 1 https://gitlab.com/AnGgIt86/nfs-clang.git -b main $CIRRUS_WORKING_DIR/CLANG
