53cb618786dc176c5c7e6acbea29aa0419175ccd
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-08-19 19:43:17 +0700

    [skip ci]

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Downloading Kernel Sources.."
-git clone --depth 1 https://github.com/NFS-projects/android_kernel_xiaomi_rosy $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth 1 https://github.com/NFS-Project/android_kernel_xiaomi_rosy $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 echo ""
 echo "Downloading Toolchain.."
 git clone --depth 1 https://github.com/kdrag0n/proton-clang -b master $CIRRUS_WORKING_DIR/CLANG
