1b40aa8d8f04b6a133db8c1abf0ad177f472d17d
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-09-02 06:04:45 +0700

    Test clang 14

--- download.sh
+++ download.sh
@@ -4,4 +4,4 @@ echo "Downloading Kernel Sources.."
 git clone --depth 1 https://github.com/NFS-Project/android_kernel_xiaomi_rosy $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 echo ""
 echo "Downloading Toolchain.."
-git clone --depth 1 https://gitlab.com/LeCmnGend/proton-clang -b clang-15 $CIRRUS_WORKING_DIR/CLANG
+git clone --depth 1 https://gitlab.com/LeCmnGend/proton-clang.git -b clang-14 $CIRRUS_WORKING_DIR/CLANG
