134be107462497537140c5a181f3289bce160572
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-09-02 06:22:11 +0700

    [skip ci]

--- done.sh
+++ done.sh
@@ -14,6 +14,5 @@ tg_post_msg() {
 cd $CIRRUS_WORKING_DIR
 
 curl -s -X POST "$BOT_MSG_URL2/sendSticker" \
--d sticker="CAACAgIAAx0CXjGT1gACAeVg69gXIw-a6h1nvmmaub51tQQwCgACLQMAAsbMYwIquW4nbs0crSAE" \
+-d sticker="CAACAgQAAx0EabRMmQACAm9jET5WwKp2FMYITmo6O8CJxt3H2wACFQwAAtUjEFPkKwhxHG8_Kx4E" \
 -d chat_id="$TG_CHAT_ID"
-tg_post_msg "<b>Building Kernel :</b><code> success</code>%0A<b>Changelog :</b>https://github.com/NFS-projects/android_kernel_xiaomi_rosy/commits/lineage-19.1"
