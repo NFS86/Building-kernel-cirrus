1ced1b2adba02fa4d2ee3838ae2c659588f5ee60
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-09-04 17:50:51 +0700

    Test kernel A13

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Downloading Kernel Sources.."
-git clone --depth 1 https://github.com/NFS-Project/android_kernel_xiaomi_rosy $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth 1 https://github.com/NFS-Project/android_kernel_xiaomi_rosy -b 13 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 echo ""
 echo "Downloading Toolchain.."
 git clone --depth 1 https://gitlab.com/LeCmnGend/proton-clang.git -b clang-14 $CIRRUS_WORKING_DIR/CLANG
