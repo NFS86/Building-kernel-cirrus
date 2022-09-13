2208668be55fb27114ed9d3e8f69717960bf096f
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-09-13 19:04:31 +0700

    Test staging kernel

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Downloading Kernel Sources.."
-git clone --depth 1 https://github.com/NFS-Project/android_kernel_xiaomi_rosy -b 13.0 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth 1 https://github.com/NFS-Project/android_kernel_xiaomi_rosy -b staging $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 echo ""
 echo "Downloading Toolchain.."
 git clone --depth 1 https://gitlab.com/AnggaR96s/clang-gengkapak.git -b main $CIRRUS_WORKING_DIR/CLANG
