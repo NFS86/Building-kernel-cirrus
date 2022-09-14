7d1a1b00809a6258ded810af1d1066f06cc64207
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-09-14 22:38:13 +0700

    Test clang

--- download.sh
+++ download.sh
@@ -4,4 +4,4 @@ echo "Downloading Kernel Sources.."
 git clone --depth 1 https://github.com/NFS-Project/android_kernel_xiaomi_rosy -b staging $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 echo ""
 echo "Downloading Toolchain.."
-git clone --depth 1 https://scm.osdn.net/gitroot/gengkapak/clang-GengKapak-16.git -b main $CIRRUS_WORKING_DIR/CLANG
+git clone --depth 1 https://pf.osdn.net/gitroot/n/nf/nfs86/NFS-clang.git -b master $CIRRUS_WORKING_DIR/CLANG
