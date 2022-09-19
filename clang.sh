7cef6728d78584c64ccfd33875c5f121738644d5
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-09-19 20:41:19 +0700

    Test LTO

--- clang.sh
+++ clang.sh
@@ -51,7 +51,12 @@ tg_post_msg "<b>Buiild Kernel started..</b>"
 make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
 	CC=${CLANG_ROOTDIR}/bin/clang \
+        AR=${CLANG_ROOTDIR}/bin/llvm-ar \
+        NM=${CLANG_ROOTDIR}/bin/llvm-nm \
+        OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
         OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
+        STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
+        LD=${CLANG_ROOTDIR}/bin/ld.lld \
 	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
 	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
    if ! [ -a "$IMAGE" ]; then
