8c62f8cf1bb3ad5ad362a5ad42e287f188d30649
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-09-20 21:25:52 +0700

    Change kernel branch and test build

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Downloading Kernel Sources.."
-git clone --depth 1 https://github.com/NFS-Project/android_kernel_xiaomi_rosy -b staging $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth 1 https://github.com/NFS-Project/android_kernel_xiaomi_rosy -b 13.0 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 echo ""
 echo "Downloading Toolchain.."
 git clone --depth 1 https://pf.osdn.net/gitroot/n/nf/nfs86/NFS-clang.git -b master $CIRRUS_WORKING_DIR/CLANG
