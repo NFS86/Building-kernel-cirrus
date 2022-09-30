f8d96b64a456b0b56e31805f6fecf0d705a89529
NFS86 <jarbull86@gmail.com>
2022-09-30 02:35:07 +0000

    Bact to NFS clang
    This reverts commit a6885c9ce0b4533d87e3adda2272c0bca9fdf8ad.

--- download.sh
+++ download.sh
@@ -4,4 +4,4 @@ echo "Downloading Kernel Sources.."
 git clone --depth 1 https://github.com/NFS-Project/android_kernel_xiaomi_rosy -b 13.0 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 echo ""
 echo "Downloading Toolchain.."
-git clone --depth 1 https://github.com/NFS-Project/proton-clang -b master $CIRRUS_WORKING_DIR/CLANG
+git clone --depth 1 https://pf.osdn.net/gitroot/n/nf/nfs86/NFS-clang.git -b master $CIRRUS_WORKING_DIR/CLANG
