2e5f13ac0ab23333e3c01a433d215ac3e7e4bbae
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-10-09 08:49:56 +0700

    Test kernel rebase

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Downloading Kernel Sources.."
-git clone --depth 1 https://github.com/NFS86/kernel_msm-4.9 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth 1 https://NFS86:${GH_TOKEN}@github.com/NFS-Project/android_kernel_xiaomi_rosy -b twelve $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 echo ""
 echo "Downloading Toolchain.."
 git clone --depth 1 https://github.com/NFS-Project/proton-clang -b master $CIRRUS_WORKING_DIR/CLANG
