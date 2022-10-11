f2314836d969222bbb5c073b234e82856775f74b
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-10-11 16:35:42 +0700

    Change branch

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Downloading Kernel Sources.."
-git clone --depth 1 https://NFS86:${GH_TOKEN}@github.com/NFS-Project/android_kernel_xiaomi_rosy -b twelve $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth 1 https://NFS86:${GH_TOKEN}@github.com/NFS-Project/android_kernel_xiaomi_rosy -b thirteen $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 echo ""
 echo "Downloading Toolchain.."
 git clone --depth 1 https://pf.osdn.net/gitroot/n/nf/nfs86/NFS-clang.git -b master $CIRRUS_WORKING_DIR/CLANG
